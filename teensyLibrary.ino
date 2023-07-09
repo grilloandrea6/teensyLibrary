a0c47e4a1c8af3722bad7a8236a268a96dcf4cf9
Andrea Grillo <grilloandrea6@gmail.com>
2023-07-09 23:01:03 +0200

    update ino example file

--- teensyLibrary.ino
+++ teensyLibrary.ino
@@ -1,9 +1,9 @@
 #include "Sensors.h"
 
-void alarmCallback(int, int);
+void alarmCallback(uint8_t, uint8_t, uint16_t);
 
 // istanza della classe Sensors, imposto soglie di default e funzione di callBack per la ricezione degli allarmi
-Sensors sensors({.yellowThreshold = 7, .redThreshold = 9, .laserThreshold = 45}, alarmCallback);
+Sensors sensors({.yellowThreshold = 1300, .redThreshold = 1200, .laserThreshold = 500, .alarmTimeout = 2000}, alarmCallback);
 
 void setup() {
   // inizializzo la classe sensori
@@ -19,8 +19,16 @@ void setup() {
   Serial.println(checkSoglie.laserThreshold);
 
   // variazione delle soglie
-  sensors.setThreshold({.yellowThreshold = 1, .redThreshold = 2, .laserThreshold = 27});
-
+  Serial.println("Impostazione nuove soglie.");
+  sensors.setThreshold({.yellowThreshold = 1500, .redThreshold = 1000, .laserThreshold = 1000, .alarmTimeout = 2000});
+  // controllo le soglie gia impostate
+  checkSoglie = sensors.getThreshold();
+  Serial.print("Soglie impostate: giallo: ");
+  Serial.print(checkSoglie.yellowThreshold);
+  Serial.print(", rosso: ");
+  Serial.print(checkSoglie.redThreshold);
+  Serial.print(", laser: ");
+  Serial.println(checkSoglie.laserThreshold);
   // richiesta distanze ad un sensore
   dist_t distanza = sensors.requestDistance(sensor1);
   if(distanza.error) {
@@ -39,17 +47,22 @@ void loop() {
 }
 
 // funzione di callback, richiamata quando si riceve un allarme
-void alarmCallback(int sensorId, int soglia){
+void alarmCallback(uint8_t sensorId, uint8_t soglia, uint16_t dist){
   if(soglia == ALARM_YELLOW) {
     Serial.print("Ricevuto allarme giallo da sensore: ");
     Serial.println(sensorId);
+    Serial.print("Distanza: ");
+    Serial.println(dist);
   }
   else if(soglia == ALARM_RED) {
     Serial.print("Ricevuto allarme rosso da sensore: ");
     Serial.println(sensorId);
+    Serial.print("Distanza: ");
+    Serial.println(dist);
   } else if(soglia == ALARM_LASER) {
     Serial.print("Ricevuto allarme laser da sensore: ");
     Serial.println(sensorId);
+    //sul laser non mando la distanza
   }
 
 
